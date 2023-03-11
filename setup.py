8eac91c6dc20eeaa7225a0eab6d73b01478ea4fb
andru.quiroga.18@cnu.edu <andru.quiroga.18@cnu.edu>
2023-03-11 03:57:02 -0500

    push

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ setup(
     include_package_data=True,
     version='0.0.1',
     packages=['SquidsTools'],
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.txt', '*.html', '*.css', '*.js', '*.csv'],
+    },
     url='https://github.com/AndruQuiroga/Squid-s-Tools',
     license='',
     author='Dreski',
