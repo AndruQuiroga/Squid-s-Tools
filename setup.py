78e9a579f5adcc658e3923a69fdf612a62ca9064
andru.quiroga.18@cnu.edu <andru.quiroga.18@cnu.edu>
2023-03-11 04:21:00 -0500

    push

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     },
     url='https://github.com/AndruQuiroga/Squid-s-Tools',
     entry_points={
-        'SquidsTools': [
+        'console_scripts': [
             'SquidsTools=SquidsTools.run:main'
         ]
     },
